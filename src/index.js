47f6cba2098f26cec1accb4bba24e47ca58d0efc
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-09-15 14:53:03 +0900

    route set

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import firebase from "./firebase";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
