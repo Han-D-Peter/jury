84a6fc1dd625cb033ca8bbdf49c92d5d68da1b99
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-09-20 16:37:35 +0900

    picture attachment

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
-import Incident from "components/Incident";
-import { dbService } from "firebaseInit";
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "firebaseInit";
+import Incident from "components/Incident";
 
 const Home = ({ userObj }) => {
   const [incident, setIncident] = useState("");
   const [incidents, setIncidents] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     dbService.collection("incidents").onSnapshot(snapshot => {
@@ -18,11 +20,18 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async event => {
     event.preventDefault();
-    await dbService.collection("incidents").add({
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+    const response = await fileRef.putString(attachment, "data_url");
+    const attachmentUrl = await response.ref.getDownloadURL();
+    const incidentObj = {
       text: incident,
-      createdAt: Date.now(),
-      createId: userObj.uid,
-    });
+      createAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl,
+    };
+    await dbService.collection("incidents").add(incidentObj);
+    setIncident("");
+    setAttachment("");
   };
   const onChange = event => {
     const {
@@ -30,6 +39,23 @@ const Home = ({ userObj }) => {
     } = event;
     setIncident(value);
   };
+
+  const onFileChange = event => {
+    const {
+      target: { files },
+    } = event;
+    const theFiles = files[0];
+    const reader = new FileReader();
+    reader.onloadend = finishedEvent => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFiles);
+  };
+
+  const onClearAttachment = () => setAttachment(null);
 
   return (
     <span>
@@ -41,8 +67,14 @@ const Home = ({ userObj }) => {
           placeholder="What's on your main?"
           maxLength={120}
         />
-
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Jury" />
+        {attachment && (
+          <div>
+            <img src={attachment} alt="*" width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {incidents.map(incident => (
